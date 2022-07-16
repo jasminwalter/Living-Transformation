28da7e38dbe4b9db9b3f030653e49526694db11a
Jasmin <jawalter@uos.de>
2022-07-16 19:21:15 +0200

    added environment and first model
    Kuppel und Platform und Materialien

---
+++ Assets/Scripts/ModelBehavior/SwitchMaterials.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMaterials : MonoBehaviour
+{
+    // public GameObject model1;
+    // public GameObject model2;
+    // public float alpha1 = 255.0f;
+    // public float alpha2 = 0.0f;
+    // public float min = 0.0f;
+    // public float max = 1.0f;
+    //
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     MeshRenderer mesh1 = model1.GetComponent<MeshRenderer>();
+    //     MeshRenderer mesh2 = model2.GetComponent<MeshRenderer>();
+    //     
+    // }
+    //
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //
+    //     
+    //     // model1.GetComponent<MeshRenderer>().material.color.a = Mathf.Lerp(min,max, alpha1);
+    //     //
+    //     // alpha1 += 0.1f * Time.deltaTime;
+    //
+    //
+    //
+    //
+    //
+    //
+    // }
+}
