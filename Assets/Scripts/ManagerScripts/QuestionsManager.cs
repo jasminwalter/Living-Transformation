984a3046db3a24015c62983fa9dd2538bed6d9bf
Jasmin <jawalter@uos.de>
2022-09-19 21:04:10 +0200

    work-in-progress of adding button interactions

---
+++ Assets/Scripts/ManagerScripts/QuestionsManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionsManager : MonoBehaviour
+{
+    public GameObject languageSelection;
+    public GameObject flagGerman;
+    public GameObject flagEnglish;
+    public GameObject textGerman;
+    public GameObject textEnglish;
+    public bool german = false;
+    public bool english = false;
+    public bool languageQuestionAnswered = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (languageQuestionAnswered)
+        {
+            // save answer
+            
+            // deactivate question and activate next one
+            languageSelection.SetActive(false);
+        }
+    }
+
+    public void LanguageSelection()
+    {
+        // save answer
+        
+        languageSelection.SetActive(false);
+        german = true;
+    }
+}
---
+++ Assets/Scripts/QuestionScripts/flagGerman.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flagGerman : MonoBehaviour
+{
+    
+    public QuestionsManager questionManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        questionManager = GameObject.Find("QuestionManager").GetComponent<QuestionsManager>();
+        
+    }
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Hand"))
+    //     {
+    //         questionManager.german = true;
+    //         questionManager.languageQuestionAnswered = true;
+    //     }
+    //     
+    // }
+    
+
+}
