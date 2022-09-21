8487725f2b30c1481af013f4eaf8fd5f20bb193a
Jasmin <jawalter@uos.de>
2022-09-21 16:14:24 +0200

    added menu language selection

--- Assets/Scripts/ManagerScripts/QuestionsManager.cs
+++ Assets/Scripts/ManagerScripts/QuestionsManager.cs
@@ -1,42 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR;
+using Valve.VR;
 
 public class QuestionsManager : MonoBehaviour
 {
     public GameObject languageSelection;
-    public GameObject flagGerman;
-    public GameObject flagEnglish;
-    public GameObject textGerman;
-    public GameObject textEnglish;
     public bool german = false;
     public bool english = false;
     public bool languageQuestionAnswered = false;
+    public bool inPrepRoom = true;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+        
         if (languageQuestionAnswered)
         {
             // save answer
-            
+            languageSelection.GetComponent<CanvasGroup>().alpha = Mathf.Lerp(1, 0, 1.1f*Time.deltaTime);
             // deactivate question and activate next one
-            languageSelection.SetActive(false);
+
+            //languageSelection.SetActive(false);
         }
     }
-
-    public void LanguageSelection()
+    #region ButtonResponses
+    #region LanguageSelection
+    public void SelectionEnglish()
     {
         // save answer
-        
-        languageSelection.SetActive(false);
-        german = true;
+        Debug.Log("Selection English");
+        english = true;
+        languageQuestionAnswered = true;
+        // fade in or fade out of 
+
     }
+
+    public void SelectionGerman()
+    {
+        Debug.Log("Selection German");
+        german = true;
+        languageQuestionAnswered = true;
+        
+    }
+    #endregion
+    
+    
+    
+
+    #endregion
+    
 }
