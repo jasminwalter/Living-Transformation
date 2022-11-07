77892b5d1bcd320c99b8f68152bee221de323fb6
Jasmin <jawalter@uos.de>
2022-11-07 15:01:19 +0100

    solo version
    remote gaze sphere controlled through right hand controller

---
+++ Assets/ControllerRemoteGazeSphere.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerRemoteGazeSphere : MonoBehaviour
+{
+
+    public GameObject remoteGazeSphere;
+    private bool simulateGazeSphereRemote = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (simulateGazeSphereRemote)
+        {
+            // StartRecording();
+            // testGazeSphere = false;
+            int layerGazeSphereLocal = 1 << LayerMask.NameToLayer("RemoteGazeSphere");
+
+            RaycastHit firstHit;
+            if (Physics.Raycast(transform.position, transform.forward, out firstHit, Mathf.Infinity))
+            {
+                remoteGazeSphere.transform.position = firstHit.point;
+            
+            }
+        }
+        
+    }
+}
