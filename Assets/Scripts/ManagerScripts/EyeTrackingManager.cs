08c21c99464583636cfbd0529771cbeeec3f05b2
Ana <akvartina@uni-osnabrueck.de>
2024-03-19 18:56:41 +0100

    Calibration Working
    But lots of errors.

--- Assets/Scripts/ManagerScripts/EyeTrackingManager.cs
+++ Assets/Scripts/ManagerScripts/EyeTrackingManager.cs
@@ -220,6 +220,8 @@ public class EyeTrackingManager : MonoBehaviour
         
         fixationPoint.SetActive(false);
         _eyeValidationDataFrames = new List<EyeValidationData>();
+
+        StartCalButton();
     }
 
     // Update is called once per frame
