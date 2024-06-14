4b017f4454cbdf4f352f043fa7bdb583af285b12
Ana <akvartina@uos.de>
2024-06-14 11:44:04 +0200

    Mirrors Updated

---
+++ Assets/Scripts/Training/MirrorBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorBehaviour : MonoBehaviour
+{
+    //References
+    public Transform head;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (head != null)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.y = head.position.y;
+            transform.position = newPosition;
+
+            Debug.Log($"(A: {transform.position.y}; B: {head.position.y})");
+        }
+    }
+}
