03c6ffe4a310d2e9b1054cefedf2c56e51c09ab7
Jasmin <jawalter@uos.de>
2024-10-22 16:04:27 +0200

    Foot IK update, avatar blendshape work in progress
    Problem with EULA Sranipal check other PC

---
+++ Assets/Scripts/AvatarMovement/AvatarEyeBlendshapes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ViveSR
+{
+    namespace anipal
+    {
+        namespace Eye
+        {
+            public class AvatarEyeBlendshapes : MonoBehaviour
+            {
+
+                /// <summary>
+                /// Customize this curve to fit the blend shapes of your avatar.
+                /// </summary>
+                [SerializeField] private AnimationCurve EyebrowAnimationCurveUpper;
+                /// <summary>
+                /// Customize this curve to fit the blend shapes of your avatar.
+                /// </summary>
+                [SerializeField] private AnimationCurve EyebrowAnimationCurveLower;
+                /// <summary>
+                /// Customize this curve to fit the blend shapes of your avatar.
+                /// </summary>
+                [SerializeField] private AnimationCurve EyebrowAnimationCurveHorizontal;
+
+                public bool NeededToGetData = true;
+                private Dictionary<EyeShape_v2, float> EyeWeightings = new Dictionary<EyeShape_v2, float>();
+                private AnimationCurve[] EyebrowAnimationCurves = new AnimationCurve[(int)EyeShape_v2.Max];
+
+                [SerializeField] private List<EyeShapeTable_v2> EyeShapeTables;
+                
+                
+            }
+        }
+    }
+}
